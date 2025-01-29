37da0662333622433b03a8cadc4681b09e608138
Tamás Gálffy <ezittgtx@gmail.com>
2025-01-29 15:33:07 +0100

    draft daemon over tcp

--- addons/vest/vest-runner.gd
+++ addons/vest/vest-runner.gd
@@ -38,3 +38,71 @@ func run_suite(suite: VestSuite) -> VestResult.Suite:
 
 func run_instance(instance: VestTest) -> VestResult.Suite:
 	return run_suite(instance._get_suite())
+
+func run_script_at(path: String) -> VestResult.Suite:
+	var test_script := load(path)
+
+	if not test_script or not test_script is Script:
+		return null
+
+	var test_instance = test_script.new()
+	if not test_instance is VestTest:
+		return null
+
+	return run_instance(test_instance)
+
+func run_in_background(instance: VestTest):
+	# Start host
+	var port = 16384
+	var server := TCPServer.new()
+
+	# Find random port for host
+	for i in range(32):
+		port = randi_range(49152, 65535)
+		if server.listen(port) == OK:
+			break
+	if not server.is_listening():
+		print("Failed to find available port!")
+		return
+
+	# Start process
+	var pid := OS.create_instance([
+		"--headless", "-s", "res://addons/vest/vest-daemon.gd",
+		"--vest-port", port,
+		"--vest-file", instance.get_script().resource_path
+	])
+	print("Started process %d" % [pid])
+
+	# Listen for connection
+	print("Waiting for incoming connection...")
+	for i in range(32):
+		if server.is_connection_available():
+			break
+		await get_tree().create_timer(0.2).timeout
+
+	if not server.is_connection_available():
+		print("Timeout!")
+		server.stop()
+		return
+
+	var peer := server.take_connection()
+
+	# Take results
+	print("Waiting for results...")
+	for i in range(32):
+		if peer.get_available_bytes() > 0:
+			break
+		await get_tree().create_timer(0.2).timeout
+
+	var results := peer.get_utf8_string()
+
+	print(results)
+
+	peer.disconnect_from_host()
+	server.stop()
+
+#	print("Waiting before killing %d" % [pid])
+#	await get_tree().create_timer(1.).timeout
+#
+#	print("Killing process %d" % [pid])
+#	OS.kill(pid)
