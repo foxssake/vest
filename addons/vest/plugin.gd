b3f2e9de643b034cfc6a92bfa99da828f36fdf01
Tamás Gálffy <ezittgtx@gmail.com>
2025-02-08 18:36:37 +0100

    render test results as tree

--- addons/vest/plugin.gd
+++ addons/vest/plugin.gd
@@ -4,12 +4,27 @@ extends EditorPlugin
 var bottom_control: VestUI
 
 func _enter_tree():
-	bottom_control = (preload("res://addons/vest/ui/vest-ui.tscn") as PackedScene).instantiate() as VestUI
-	resource_saved.connect(bottom_control.handle_resource_saved)
+	bottom_control = _create_ui()
+#	resource_saved.connect(bottom_control.handle_resource_saved)
+#	resource_saved.connect(_refresh_ui)
 
 	add_control_to_bottom_panel(bottom_control, "Vest")
 
 func _exit_tree():
-	resource_saved.disconnect(bottom_control.handle_resource_saved)
+#	resource_saved.disconnect(bottom_control.handle_resource_saved)
+#	resource_saved.disconnect(_refresh_ui)
 	remove_control_from_bottom_panel(bottom_control)
 	bottom_control.queue_free()
+
+func _refresh_ui(__):
+	if bottom_control:
+		remove_control_from_bottom_panel(bottom_control)
+		bottom_control.queue_free()
+
+	bottom_control = _create_ui()
+	add_control_to_bottom_panel(bottom_control, "Vest")
+
+	resource_saved.connect(bottom_control.handle_resource_saved)
+
+func _create_ui() -> VestUI:
+	return (preload("res://addons/vest/ui/vest-ui.tscn") as PackedScene).instantiate() as VestUI
--- addons/vest/ui/vest-ui.gd
+++ addons/vest/ui/vest-ui.gd
@@ -5,7 +5,7 @@ class_name VestUI
 @onready var run_all_button := %"Run All Button" as Button
 @onready var run_on_save_checkbox := %"Run On Save CheckBox" as CheckBox
 @onready var clear_button := %"Clear Button" as Button
-@onready var results_container := %Results as VBoxContainer
+@onready var results_tree := %"Results Tree" as Tree
 @onready var summary_label := %"Tests Summary Label" as Label
 @onready var results_label := %"Tests Result Label" as Label
 
@@ -32,28 +32,17 @@ func run_all():
 	var fail_row = load("res://addons/vest/ui/fail-row.tscn") as PackedScene
 
 	# Render individual results
-	var output_label := Label.new()
-	results_container.add_child(output_label)
-	output_label.text = TAPReporter.report(results)
-#	for test_result in results:
-#		var result_row: VestResultRow
-#		if test_result.is_success():
-#			result_row = success_row.instantiate()
-#		else:
-#			result_row = fail_row.instantiate()
-#
-#		results_container.add_child(result_row)
-#		result_row.set_result(test_result)
+	_render_result(results, results_tree)
+	var item := results_tree.create_item()
 
 	# Render summaries
 	summary_label.text = "Ran %d tests in %.2fms" % [results.size(), test_duration * 1000.]
-#	results_label.text = ("%s %s" % [VestTest.status_emoji(aggregate_result), VestTest.status_string(aggregate_result)]).capitalize()
+	results_label.text = ("%s" % [results.get_aggregate_status_string()]).capitalize()
 
 func clear_results():
-	var result_rows := results_container.get_children()
-	for row in result_rows:
-		results_container.remove_child(row)
-		row.queue_free()
+	results_tree.clear()
+	summary_label.text = ""
+	results_label.text = ""
 
 func _ready():
 	run_all_button.pressed.connect(run_all)
@@ -61,6 +50,32 @@ func _ready():
 		_run_on_save = toggled
 	)
 	clear_button.pressed.connect(clear_results)
+
+func _render_result(what: Object, tree: Tree, parent: TreeItem = null):
+	if what is VestResult.Suite:
+		var item := tree.create_item(parent)
+		item.set_text(0, what.suite.name)
+		item.set_text(1, what.get_aggregate_status_string().capitalize())
+
+		for subsuite in what.subsuites:
+			_render_result(subsuite, tree, item)
+		for case in what.cases:
+			_render_result(case, tree, item)
+		for benchmark in what.benchmarks:
+			_render_result(benchmark, tree, item)
+	elif what is VestResult.Case:
+		var item := tree.create_item(parent)
+		item.set_text(0, what.case.description)
+		item.set_text(1, what.get_status_string().capitalize())
+	elif what is VestResult.Benchmark:
+		var item := tree.create_item(parent)
+		item.set_text(0, what.benchmark.description)
+		item.set_collapsed_recursive(true)
+
+		tree.create_item(item).set_text(0, "Duration: %.2fms" % [what.duration * 1000.])
+		tree.create_item(item).set_text(0, "Iterations: %d" % [what.iterations])
+	else:
+		push_error("Rendering unknown object: %s" % [what])
 
 func _time() -> float:
 	return Time.get_unix_time_from_system() / 1000.
