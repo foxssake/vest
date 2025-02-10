62ababe709b3191a67d75c57cc3857b999ca6440
Tamás Gálffy <ezittgtx@gmail.com>
2025-02-10 22:14:31 +0100

    refactor, respond over network

--- addons/vest/vest-cli.gd
+++ addons/vest/vest-cli.gd
@@ -5,6 +5,8 @@ class Params:
 	var run_glob: String = "res://*.test.gd"
 	var report_format: String = ""
 	var report_file: String = ""
+	var host: String = ""
+	var port: int = -1
 
 	func validate() -> Array[String]:
 		var result: Array[String] = []
@@ -12,6 +14,8 @@ class Params:
 			result.append("No tests specified!")
 		if report_format not in ["", "tap"]:
 			result.append("Unknown report format \"%s\"!" % report_format)
+		if port != -1 and port < 0 or port > 65535:
+			result.append("Specified port %d is invalid!" % port)
 		return result
 
 	static func parse(args: Array[String]) -> Params:
@@ -29,10 +33,16 @@ class Params:
 				result.report_file = val
 			elif arg == "--vest-report-format":
 				result.report_format = val
+			elif arg == "--vest-port":
+				result.port = val.to_int()
+			elif arg == "--vest-host":
+				result.host = val
 
 		return result
 
 func _init():
+	Vest._register_scene_tree(self)
+
 	var params := Params.parse(OS.get_cmdline_args())
 	var validation_errors := params.validate()
 	if not validation_errors.is_empty():
@@ -41,6 +51,18 @@ func _init():
 		quit(1)
 		return
 
+	var results := _run_tests(params)
+	_report(params, results)
+	await _send_results_over_network(params, results)
+
+	if results.get_aggregate_status() == VestResult.TEST_PASS:
+		print_rich("All tests [color=green]passed[/color]!")
+		quit(0)
+	else:
+		push_error("There are test failures!")
+		quit(1)
+
+func _run_tests(params: Params) -> VestResult.Suite:
 	var runner := VestLocalRunner.new()
 	root.add_child(runner)
 
@@ -52,12 +74,39 @@ func _init():
 
 	runner.free()
 
+	return results
+
+func _report(params: Params, results: VestResult.Suite):
 	var report := TAPReporter.report(results)
-	print(report)
 
-	if params.report_file and params.report_format:
-		var fa := FileAccess.open(params.report_file, FileAccess.WRITE)
-		fa.store_string(report)
-		fa.close()
+	if params.report_format:
+		if params.report_file in ["", "-"]:
+			print(report)
+		else:
+			var fa := FileAccess.open(params.report_file, FileAccess.WRITE)
+			fa.store_string(report)
+			fa.close()
 
-	quit(0)
+func _send_results_over_network(params: Params, results: VestResult.Suite):
+	if not params.host and params.port == -1:
+		return
+
+	var host := params.host
+	var port := params.port
+
+	if not host: host = "0.0.0.0"
+	if port == -1: port = 54932
+
+	var peer := StreamPeerTCP.new()
+	var err := peer.connect_to_host("0.0.0.0", port)
+	if err != OK:
+		push_warning("Couldn't connect on port %d! %s" % [port, error_string(err)])
+		return
+
+	await Vest.until(func(): return peer.get_status() != StreamPeerTCP.STATUS_CONNECTING)
+	if peer.get_status() != StreamPeerTCP.STATUS_CONNECTED:
+		push_warning("Connection timed out! Socket status: %d" % [peer.get_status()])
+		return
+
+	peer.put_var(results._to_wire(), true)
+	peer.disconnect_from_host()
--- addons/vest/vest-singleton.gd
+++ addons/vest/vest-singleton.gd
@@ -2,12 +2,31 @@ extends Object
 class_name Vest
 
 static var _messages: Array[String] = []
+static var _scene_tree: SceneTree
 
 static func print(message: String):
 	_messages.append(message)
+
+static func until(condition: Callable, timeout: float = 5., interval: float = 0.2) -> Error:
+	if not _scene_tree:
+		push_warning("Missing reference to SceneTree, will return immediately!")
+		return ERR_UNAVAILABLE
+
+	while timeout > 0.:
+		if condition.call():
+			break
+		await _scene_tree.create_timer(interval).timeout
+		timeout -= interval
+
+	if not condition.call():
+		return ERR_TIMEOUT
+	return OK
 
 static func _clear_messages():
 	_messages.clear()
 
 static func _get_messages() -> Array[String]:
 	return _messages.duplicate()
+
+static func _register_scene_tree(scene_tree: SceneTree):
+	_scene_tree = scene_tree
