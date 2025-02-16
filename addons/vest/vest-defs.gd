a22716e00396d8e1347676ed9bb3ca611d002295
Tamás Gálffy <ezittgtx@gmail.com>
2025-02-16 22:47:46 +0100

    touchup

--- addons/vest/vest-defs.gd
+++ addons/vest/vest-defs.gd
@@ -48,3 +48,63 @@ class Case:
 		result.definition_line = data["definition_line"]
 
 		return result
+
+class Benchmark:
+	var callback: Callable
+	var name: String = ""
+	var iterations: int = 0
+	var duration: float = 0.0
+
+	var max_iterations: int = -1
+	var max_duration: float = -1.0
+
+	var _test: VestTest
+
+	func with_iterations(p_iterations: int) -> Benchmark:
+		max_iterations = p_iterations
+		return self
+
+	func with_duration(p_duration: float) -> Benchmark:
+		max_duration = p_duration
+		return self
+
+	func once() -> Benchmark:
+		max_iterations = 1
+		max_duration = 0.0
+		return run()
+
+	func run() -> Benchmark:
+		# Run benchmark
+		while _is_within_limits():
+			var t_start := Vest.time()
+			callback.call()
+
+			duration += Vest.time() - t_start
+			iterations += 1
+
+		# Report
+		var result_data := _test._get_result().data
+		var benchmarks := result_data.get("benchmarks", []) as Array
+		benchmarks.append(_to_data())
+		result_data["benchmarks"] = benchmarks
+
+		# Set test to pass by default if no asserts are added
+		_test.ok("", result_data)
+
+		return self
+
+	func _is_within_limits():
+		if max_iterations >= 0 and iterations >= max_iterations:
+			return false
+		if max_duration >= 0.0 and duration >= max_duration:
+			return false
+		return true
+
+	func _to_data() -> Dictionary:
+		var result := {}
+		result["name"] = name
+		result["iterations"] = iterations
+		result["duration"] = "%.4fms" % [duration * 1000.0]
+		result["iters/sec"] = iterations / duration
+		result["average iteration time"] = "%.4fms" % [duration / iterations * 1000.0]
+		return result
--- examples/benchmarks.test.gd
+++ examples/benchmarks.test.gd
@@ -3,15 +3,52 @@ extends VestTest
 func get_suite_name() -> String:
 	return "Benchmarks"
 
-func test_string_concatenation():
-	var line := "a".repeat(128) + "\n"
-	var buffer_string := ""
-	var buffer_array := PackedStringArray()
+func suite():
+	test("Random ID generation", func():
+		var length := 16
+		var charset := "abcdefghijklmnopqrstuvwxyz0123456789"
 
-	measure("String", func():
-		buffer_string += line
-	).with_iterations(16384).run()
+		benchmark("String concatenation", func():
+			var id := ""
+			for i in range(length):
+				id += charset[randi() % charset.length()]
+		).with_iterations(1_000).run()
 
-	measure("PackedStringArray", func():
-		buffer_array.append(line)
-	).with_iterations(16384).run()
+		benchmark("Range mapping", func():
+			var id := "".join(
+				range(length)
+					.map(func(__): return charset[randi() % charset.length()])
+				)
+		).with_iterations(1_000).run()
+
+		benchmark("PackedStringArray", func():
+			var chars := PackedStringArray()
+			for i in range(length):
+				chars.append(charset[randi() % charset.length()])
+			var id := "".join(chars)
+		).with_iterations(1_000).run()
+	)
+
+func test_random_id_generation():
+	var length := 16
+	var charset := "abcdefghijklmnopqrstuvwxyz0123456789"
+
+	benchmark("String concatenation", func():
+		var id := ""
+		for i in range(length):
+			id += charset[randi() % charset.length()]
+	).with_iterations(1_000).run()
+
+	benchmark("Range mapping", func():
+		var id := "".join(
+			range(length)
+				.map(func(__): return charset[randi() % charset.length()])
+			)
+	).with_iterations(1_000).run()
+
+	benchmark("PackedStringArray", func():
+		var chars := PackedStringArray()
+		for i in range(length):
+			chars.append(charset[randi() % charset.length()])
+		var id := "".join(chars)
+	).with_iterations(1_000).run()
