ac49a64c4054dd38be9d4652e1854f0fd78626c3
Tamás Gálffy <ezittgtx@gmail.com>
2025-02-17 13:29:46 +0100

    docs: Capturing signals (#67)
    Closes #51

---
+++ examples/signals.test.gd
@@ -0,0 +1,47 @@
+extends VestTest
+
+signal on_event(data: String)
+
+func get_suite_name() -> String:
+	return "Signals test"
+
+func before_all():
+	capture_signal(on_event, 1)
+
+func suite():
+	test("Signal was emitted at least once", func():
+		on_event.emit("foo")
+		on_event.emit("bar")
+
+		expect_not(get_signal_emissions(on_event).is_empty())
+	)
+
+	test("Signal was emitted just once", func():
+		on_event.emit("foo")
+
+		expect_equal(get_signal_emissions(on_event).size(), 1)
+	)
+
+	test("Signal was emitted with params", func():
+		on_event.emit("foo")
+		on_event.emit("bar")
+
+		expect_contains(get_signal_emissions(on_event), ["bar"])
+	)
+
+func test_signal_was_emitted_at_least_once():
+	on_event.emit("foo")
+	on_event.emit("bar")
+
+	expect_not(get_signal_emissions(on_event).is_empty())
+
+func test_signal_was_emitted_just_once():
+	on_event.emit("foo")
+
+	expect_equal(get_signal_emissions(on_event).size(), 1)
+
+func test_signal_was_emitted_with_params():
+	on_event.emit("foo")
+	on_event.emit("bar")
+
+	expect_contains(get_signal_emissions(on_event), ["bar"])
