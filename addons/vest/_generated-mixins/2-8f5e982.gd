f95dbd71787635b7453d9d523269e883fd7a2394
Tamás Gálffy <ezittgtx@gmail.com>
2025-03-19 23:15:33 +0100

    feat: Null asserts (#104)
    Closes #100

--- addons/vest/_generated-mixins/2-8f5e982.gd
+++ addons/vest/_generated-mixins/2-8f5e982.gd
@@ -114,6 +114,20 @@ func expect_doesnt_contain(object: Variant, item: Variant, p_message: String = "
 		ERR_CANT_RESOLVE:
 			fail("Unknown object, can't be checked if it contains item!", { "object": object })
 
+## Expect a [param value] to be null.
+func expect_null(value: Variant, p_message: String = "Item is not null!") -> void:
+	if value == null:
+		ok()
+	else:
+		fail(p_message, { "got": value })
+
+## Expect a [param value] to not be null.
+func expect_not_null(value: Variant, p_message: String = "Item is null!") -> void:
+	if value != null:
+		ok()
+	else:
+		fail(p_message)
+
 func _is_equal(actual, expected) -> bool:
 	if actual is Object and actual.has_method("equals"):
 		return actual.equals(expected)
