ec3bc3dbce4c8b95179bcd3e93a0fe4266f521da
Tamás Gálffy <ezittgtx@gmail.com>
2025-03-22 23:12:39 +0100

    feat: Support all built-in types for asserts (#112)
    Closes #106

--- addons/vest/test/mixins/assert-mixin.gd
+++ addons/vest/test/mixins/assert-mixin.gd
@@ -129,7 +129,7 @@ func _is_equal(actual, expected) -> bool:
 	return actual == expected
 
 func _is_empty(object: Variant) -> Variant:
-	if object is Array or object is Dictionary:
+	if _is_builtin_container(object) or _is_stringlike(object):
 		return object.is_empty()
 	elif object is Object:
 		if object.has_method("is_empty"):
@@ -140,8 +140,10 @@ func _is_empty(object: Variant) -> Variant:
 		return ERR_CANT_RESOLVE
 
 func _contains(object: Variant, item: Variant) -> Variant:
-	if object is Array or object is Dictionary:
+	if _is_builtin_container(object):
 		return object.has(item)
+	elif _is_stringlike(object):
+		return object.contains(str(item))
 	elif object is Object:
 		if object.has_method("has"):
 			return object.has(item)
@@ -149,3 +151,14 @@ func _contains(object: Variant, item: Variant) -> Variant:
 			return ERR_METHOD_NOT_FOUND
 	else:
 		return ERR_CANT_RESOLVE
+
+func _is_builtin_container(object: Variant) -> bool:
+	return object is Array or object is Dictionary \
+		or object is PackedByteArray or object is PackedColorArray \
+		or object is PackedFloat32Array or object is PackedFloat64Array \
+		or object is PackedInt32Array or object is PackedInt64Array \
+		or object is PackedStringArray \
+		or object is PackedVector2Array or object is PackedVector3Array
+
+func _is_stringlike(object: Variant) -> bool:
+	return object is String or object is StringName or object is NodePath
