38547b2258667074ba9df06ca6f1e0c957896215
Tamás Gálffy <ezittgtx@gmail.com>
2025-03-30 23:41:45 +0200

    iterable

--- tests/generators.test.gd
+++ tests/generators.test.gd
@@ -4,18 +4,24 @@ func get_suite_name():
 	return "Generators"
 
 func test_generator():
+	await Vest.sleep(.2)
+	
 	var generator := Generator.of(func(gen: Callable):
 		print("Generator started!")
-		print("await gen.call(1)"); await gen.call(1)
-		print("await gen.call(2)"); await gen.call(2)
-		print("await gen.call(3)"); await gen.call(3)
+		await gen.call(1)
+		await Vest.sleep(0.1)
+		await gen.call(2)
+		await Vest.sleep(0.1)
+		await gen.call(3)
 	)
 	
 	var values := []
-	for val in generator:
-		values.append(await val)
+	for __ in generator:
+		var val = await __
+		values.append(val)
+		print(val)
 	
-	expect_equal(values, [1, 2, 3, null])
+	expect_equal(values, [1, 2, 3])
 
 func test_iterator():
 	var iterator := ThinkingIterable.new()
@@ -39,45 +45,33 @@ class Generator:
 	
 	func _init(p_callable: Callable):
 		_callable = p_callable
+		_run()
 	
 	func _generate(value: Variant) -> void:
-		print("_generate() / Queued value: %s" % [value]); _queued_value = value
-		print("_generate() / _on_push.emit()"); (func(): _on_push.emit()).call_deferred()
-		print("_generate() / await _on_pull"); await _on_pull
-		print("_generate() / _generate(%s) finished" % [value])
+		_queued_value = value
+		_status = PENDING
+		(func(): _on_push.emit()).call_deferred()
+		await _on_pull
 	
 	func _run() -> void:
-		print("_run() / _status = ACTIVE"); _status = ACTIVE
-		print("_run() / await _callable.call()"); await _callable.call(_generate)
-		print("_run() / _status = FINISHED"); _status = FINISHED
+		_status = ACTIVE
+		await _callable.call(_generate)
+		_status = FINISHED
 		
-		print("_run() / _queued_value = null"); _queued_value = null
-		print("_run() / _on_push.emit()"); (func(): _on_push.emit()).call_deferred()
+		_queued_value = null
 	
 	func _iter_init(arg) -> bool:
-		return _status == PENDING
+		return _status != FINISHED
 
 	func _iter_next(arg) -> bool:
+		_on_pull.emit()
 		return _status != FINISHED
 
 	func _iter_get(arg) -> Variant:
-		return await get_value()
-	
-	func get_value() -> Variant:
-		print("get_value()")
-		match _status:
-			PENDING:
-				print("get_value() / PENDING / _run()"); _run()
-				print("get_value() / PENDING / await _on_push"); await _on_push
-				print("get_value() / PENDING / return %s" % [_queued_value]); return _queued_value
-			ACTIVE:
-				print("get_value() / ACTIVE / _on_pull.emit()"); _on_pull.emit()
-				print("get_value() / ACTIVE / await _on_push"); await _on_push
-				print("get_value() / ACTIVE / Resulting state: %s" % [_status]);
-				print("get_value() / ACTIVE / return %s" % [_queued_value]); return _queued_value
-			_:
-				return null
-	
+		if _status == PENDING:
+			await _on_push
+		return _queued_value
+
 	static func of(p_callable: Callable) -> Generator:
 		return Generator.new(p_callable)
 
